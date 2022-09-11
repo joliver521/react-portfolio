c9e3be6e31bac446167dacff496496ef85920e07
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-09-11 19:53:41 -0400

    Header index.js updated/App.js updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
 
 function App() {
-    return <div></div>;
+    const [currentTab, setCurrentTab] = useState('about');
+
+    const tabRender = () => {
+        switch (currentTab) {
+            case 'contact':
+                return <Contact />;
+            default:
+                return null;
+        }
+    };
+    return (
+        <div>
+            <div>
+                <header
+                    currentTab={currentTab}
+                    setCurrentTab={setCurrentTab}
+                ></header>
+            </div>
+            <div>
+                <main></main>
+            </div>
+            <div>
+                <footer></footer>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header(props) {
+    const { currentTab, setCurrentTab } = props;
+
+    return (
+        <header>
+            <div>
+                <h1>Jason Oliver's React Portfolio</h1>
+            </div>
+            <div>
+                <Nav currentTab={currentTab} setCurrentTab={setCurrentTab} />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
